a30e113676499691bc3a9c3d446c95a93587dcec
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-08 13:23:49 +0200

    Svolto esercizio

--- Avanzato/Sezione4/Esercizio4-2.js
+++ Avanzato/Sezione4/Esercizio4-2.js
@@ -2,21 +2,30 @@
 che crei un oggetto Macchina con 4 proprietà:
 marca, modello e colore
 ed una proprietà che usando un'arrow function descriva la macchina
-creare 2 oggetti macchina che diano in output la propria descizione.
+Creare 2 oggetti macchina che diano in output la propria descizione.
+Creare 1 oggetto macchina che appartenga ad un altro gruppo automotive.
+Visualizzare il percorso assoluto del file html che richiama lo script
 */
 function Macchina(marca, modello, colore){
 this.marca=marca;
 this.modello=modello;
 this.colore=colore;
-this.descrizione = () =>`La mia marca è ${this.marca}, il mio modello è ${this.modello}, il mio colore è ${this.colore}, ${this.gruppo} `;
+this.gruppo="FCA";
+this.descrizione = () =>`${this.marca} ${this.modello} ${this.colore}, Gruppo: ${this.gruppo} `;
 }
-Macchina.prototype.gruppo = "faccio parte del gruppo FCA";
 
-let macchina1 = new Macchina("Ferrari","F40","Rosso")
-console.log(macchina1);
+let macchina1 = new Macchina("Ferrari","F40","rossa")
 console.log(macchina1.descrizione());
 
-let macchina2 = new Macchina("Fiat","Punto","Blu")
+let macchina2 = new Macchina("Fiat","Punto","blu")
+
+console.log(macchina2.descrizione());
+
+let macchina3 = new Macchina("Kia","ceed","grigia")
+macchina3.gruppo="HKA";
+console.log(macchina3.descrizione());
+
+console.log(macchina1);
 console.log(macchina2);
-console.log(macchina2.descrizione()); // errore verificare
-console.log(macchina2.__proto__);
+console.log(macchina3);
+console.log(window.location.href);
