72a204e065592a781eadd6eabd9470be692a5598
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-10 07:18:34 +0200

    continua lo studio suglio oggetti

--- Lezioni/S4_Oggetti/48c.js
+++ Lezioni/S4_Oggetti/48c.js
@@ -1,18 +1,23 @@
+let macchine=[];
 function Ferrari(){
 }
 Ferrari.prototype.marchio = `\u{00A9} Ferrari`;
+macchine.push(Ferrari);
 let ferrari_458 = new Ferrari();
+macchine.push(ferrari_458);
 ferrari_458.cilindrata=1600;
 ferrari_458.prototype={cavalli:160};
 let ferrari_458speciale = Object.create(ferrari_458);
-let macchine=[Ferrari,ferrari_458,ferrari_458speciale]
+macchine.push(ferrari_458speciale);
+console.log(macchine);
+//let macchine=[Ferrari,ferrari_458,ferrari_458speciale]
 for (var variable of macchine) {
-console.log("Mostra l'oggetto");
+console.log(macchine.pop(variable));
   console.log(variable);
   console.log(".prototype");
   console.log(variable.prototype);
   console.log(".__proto__");
   console.log(variable.__proto__);
-  console.log(".__proto__.__proto__");
-  console.log(variable.__proto__.__proto__);
+  // console.log(".__proto__.__proto__");
+  // console.log(variable.__proto__.__proto__);
 }
