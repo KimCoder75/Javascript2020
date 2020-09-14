87296992f0508e6c4278dc39772fc9d007139aa6
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-14 07:50:38 +0200

    configurazione xammp

--- Esercizi/7-esercizi-oggetti/2-Medi/rubrica.js
+++ Esercizi/7-esercizi-oggetti/2-Medi/rubrica.js
@@ -3,30 +3,32 @@
   Scrivi un programma per la gestione di una rubrica telefonica.
 */
 //Array contenente i nomi da trasformare in oggetti
-let namesList = ["Kim", "Matteo Gabriele", "Erika Valentina", "Rebecca", "Camilla","Matteo", "Stefano","Marco"],
-    addressBook=[]; //array contenente gli oggetti nomi
+let namesList = ["Kim", "Matteo Gabriele", "Erika Valentina", "Rebecca", "Stefano","Marco"],
+    addressBook=[];
 
 //constructor function generazione utenti
 function User(name, phoneNumber){
-   this.nome=name,
-   this.telefono=phoneNumber,
-   this.summary = () => console.log(this.nome, this.telefono)
- }
+    this.nome=name,
+    this.telefono=phoneNumber
+    this.summary = `${this.nome} ${this.telefono}`;//semplificazione da funzione a stringa
+}
 
 //inserimento di type nel prototype di User
 User.prototype.type="Familiari e Amici";
 
-//funzione generante rubrica dal lista nomi e generatore di numero telefonico
-function userFromArray(){
-  for (var us of namesList) {
-    let id=0,
-        pn = Math.floor(Math.random()*4000)+7530000,
-        phone=("02"+pn),
-        namesList[id]=new User(us,phone);
-        addressBook.push(generatedUser);
-        console.log(addressBook[id].summary());
-        id+=1
-  }
-}
-userFromArray();
+//Generazione rubrica da array nomi numero telefonico
+// utilizzo di forEach per utilizzare anche l'indice
+namesList.forEach((currentValue, index) => {
+  let pn = Math.floor(Math.random()*4000)+7530000,
+      phone=("02"+pn),
+      obj = new User(currentValue, phone);
+      addressBook.push(obj);
+console.log(addressBook[index].summary);
+});
+
+//inserimento extra in onore dell'istruttore ;)
+let gianluca = new User("Gianluca e non Giuseppe!","027530857");
+addressBook.push(gianluca);
+console.log(addressBook[6].summary);
 console.table(addressBook);
+console.log(addressBook[6].type);
--- Esercizi/7-esercizi-oggetti/2-Medi/rubrica2.js
+++ Esercizi/7-esercizi-oggetti/2-Medi/rubrica2.js
@@ -2,38 +2,31 @@
   La mia rubrica
   Scrivi un programma per la gestione di una rubrica telefonica.
 */
-let namesList = ["Kim", "Matteo Gabriele", "Erika Valentina", "Rebecca", "Stefano","Marco"],
-    addressBook=[];
+//Array contenente i nomi da trasformare in oggetti
+let namesList = ["Kim", "Matteo Gabriele", "Erika Valentina", "Rebecca", "Camilla","Matteo", "Stefano","Marco"],
+    addressBook=[]; //array contenente gli oggetti nomi
 
+//constructor function generazione utenti
 function User(name, phoneNumber){
-    this.nome=name,
-    this.telefono=phoneNumber
-    this.summary = `${this.nome} ${this.telefono}`;
+   this.nome=name,
+   this.telefono=phoneNumber,
+   this.summary = () => console.log(this.nome, this.telefono)
+ }
+
+//inserimento di type nel prototype di User
+User.prototype.type="Familiari e Amici";
+
+//funzione generante rubrica dal lista nomi e generatore di numero telefonico
+function userFromArray(){
+  for (var us of namesList) {
+    let id=0,
+        pn = Math.floor(Math.random()*4000)+7530000,
+        phone=("02"+pn),
+        namesList[id]=new User(us,phone);
+        addressBook.push(generatedUser);
+        console.log(addressBook[id].summary());
+        id+=1
+  }
 }
-
-// arr.forEach(callback(currentValue [, index [, array]])[, thisArg]);
-namesList.forEach((currentValue, index) => {
-  let pn = Math.floor(Math.random()*4000)+7530000,
-      phone=("02"+pn),
-      obj = new User(currentValue, phone);
-      addressBook.push(obj);
-console.log(addressBook[index].summary);
-
-});
-
-let gianluca = new User("Gianluca","027530857");
-addressBook.push(gianluca);
-console.log(addressBook[6].summary);
-//
-//
-// for(let name of namesList) {
-//   let pn = Math.floor(Math.random()*4000)+7530000,
-//       phone=("02"+pn),
-//       obj = new User(name, phone);
-//       addressBook[]
-//       //addressBook[obj.nome] = obj;
-//       console.log(addressBook[obj.nome]);
-// }
+userFromArray();
 console.table(addressBook);
-// addressBook.Kim.summary();
-// addressBook['Matteo Gabriele'].summary();
