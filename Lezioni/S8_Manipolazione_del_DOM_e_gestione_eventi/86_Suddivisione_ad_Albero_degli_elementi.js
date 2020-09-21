fec263bb9b9428371c8b70ed091f3fb6d69eaa5c
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-21 13:27:41 +0200

    Avanti con il DOM

--- Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/86_Suddivisione_ad_Albero_degli_elementi.js
+++ Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/86_Suddivisione_ad_Albero_degli_elementi.js
@@ -1,8 +1,10 @@
 /* Suddisione ad albero degli elementi
-gli spazi "sono considerati nodi"?
+Icommenti sono considerati childNodes
+ma non ChildElements
 */
 Ducument
-ha 3 figli
+ha 3 figli ma solo
+1 child Element: il tag html
 <!DOCTYPE html>
 lo spazio tra doctype e html
 <html lang="it" dir="ltr">
--- Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/87_Esaminiamo_l'oggetto_document.js
+++ Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/87_Esaminiamo_l'oggetto_document.js
@@ -1,1 +1,23 @@
-console.log(document);
+let info;
+info = document.title
+info = document.body
+info = document.head
+info = document.charset
+info = document.childNodes
+info = document.children
+info = document.domain
+info = document.forms
+info = document.images
+info = document.links
+info = document.links[0]
+info = document.links[1]
+info = document.links.length
+info = document.links.item(0)
+info = document.links.item(1)
+info = document.children[0]
+info = document.children[0].children;
+info = document.children[0].children[1].children;
+// info = document.children[0].children[1].children.length
+
+
+console.log(info);
