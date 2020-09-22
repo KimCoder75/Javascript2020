d4d3430ab1d8130ee76b40f0f7d3f6d85425705e
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-22 08:08:30 +0200

    Ripasso fino a 89

--- Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/87_Esaminiamo_l'oggetto_document.js
+++ Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/87_Esaminiamo_l'oggetto_document.js
@@ -9,14 +9,14 @@ info = document.domain
 info = document.forms
 info = document.images
 info = document.links
-info = document.links[0]
-info = document.links[1]
-info = document.links.length
-info = document.links.item(0)
-info = document.links.item(1)
+// info = document.links[0]
+// info = document.links[1]
+// info = document.links.length
+// info = document.links.item(0)
+// info = document.links.item(1)
 info = document.children[0]
 info = document.children[0].children;
-info = document.children[0].children[1].children;
+info = document.children[0].children[0].children;
 // info = document.children[0].children[1].children.length
 
 
