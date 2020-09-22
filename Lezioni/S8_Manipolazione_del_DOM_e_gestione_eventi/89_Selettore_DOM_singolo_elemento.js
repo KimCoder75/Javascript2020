f0bdc5c72a5c1a03314f86f567f0b16f94ce8a54
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-22 17:36:23 +0200

    Alri esperimenti col DOM

--- Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/89_Selettore_DOM_singolo_elemento.js
+++ Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/89_Selettore_DOM_singolo_elemento.js
@@ -1,15 +1,15 @@
 const sezione = document.getElementById('section1');
-let info;
-info = sezione.id;
-info = sezione.className;
-info = sezione.childElementCount;
-info = sezione.childNodes;
-info = sezione.children;
-sezione.children[1].className = 'c2';
-info = sezione.clientHeight;
-info = sezione.clientWidth;
-// sezione.style.backgroundColor = "#da12da";
-sezione.style.padding ="25px";
+// let info;
+// info = sezione.id;
+// info = sezione.className;
+// info = sezione.childElementCount;
+// info = sezione.childNodes;
+// info = sezione.children;
+// sezione.children[1].className = 'c2';
+// info = sezione.clientHeight;
+// info = sezione.clientWidth;
+// // sezione.style.backgroundColor = "#da12da";
+// sezione.style.padding ="25px";
 
 // div1.innerText ="Primo Blocco";
 // div1.innerHTML = "Primo Blocco con <a href='index.html'>link1</a>";
@@ -25,11 +25,34 @@ sezione.style.padding ="25px";
 // info[6].textContent="Ciao";
 // console.dir(info);
 
-// Var 2
-let sectionNodes = document.getElementById('section1').childNodes;
-for (let i = 0; i < sectionNodes.length; i++) {
-   if(i%2==0){
-     sectionNodes[i].innerText="Ciao";
+// let info = sezione.childNodes;
+// for (let i = 0; i < info.length; i++) {
+//    if(i%2==0){
+//      info[i].textContent="Ciao";
+// }
+// }
+// console.dir(info);
+//
+let info = sezione.childNodes;
+for (let i = 0; i < info.length; i++) {
+   if(info[i].tagName!='DIV'){
+      info[i].textContent="Ciao";
+}  else{
+      info[i].className='c2';
+      info[i].textContent='Arrivederci';
 }
 }
-console.dir(sectionNodes);
+console.dir(info);
+
+
+
+
+//
+// // Var 2
+// let sectionNodes = document.getElementById('section1').childNodes;
+// for (let i = 0; i < sectionNodes.length; i++) {
+//    if(i%2==0){
+//      sectionNodes[i].innerText="Ciao";
+// }
+// }
+// console.dir(sectionNodes);
