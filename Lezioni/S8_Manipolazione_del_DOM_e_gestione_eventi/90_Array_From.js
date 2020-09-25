c2b7728d0ebabb8c2d212fa1640534b9befe64ce
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-25 12:06:39 +0200

    Array.from e css

--- Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/90_Array_From.js
+++ Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/90_Array_From.js
@@ -1,17 +1,10 @@
-// let sezione = document.getElementById('section1');
-// let links = document.getElementsByTagName('a');
-// let linksPari = document.querySelectorAll('header > nav >a:nth-child(even)');
-//
-//
-// // linksPari.forEach((item, i) => {
-// //   item.style.background="blue";
-// //   item.style.color="white";
-// // });
-//
-// let b1 = sezione.getElementsByClassName('b1');
-// b1 = Array.from(b1);
-// //
-// // console.log(b1.reverse());
-// console.dir(b1);
-// console.dir(links);
-// console.log(linksPari);
+let sectionDivInnerText=[],
+    blocchi = document.getElementById('ElencoBlocchi').children,
+    blocchiElenco = Array.from(blocchi);
+
+    blocchiElenco.forEach((item, i) => {
+        console.log(item.innerText);
+        sectionDivInnerText.push(item.innerText);
+        item.innerText=`Blocco con indice ${i}`;
+        console.log(`l'innerText è stato cambiato da '${sectionDivInnerText[i]}' a '${item.innerText}'`)
+    });
