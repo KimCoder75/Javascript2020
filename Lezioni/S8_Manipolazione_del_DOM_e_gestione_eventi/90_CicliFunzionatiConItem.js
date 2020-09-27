ff49dea075c5861ad8b5254c859e6d85990d0e42
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-27 18:55:32 +0200

    for in usando le proprietà enumerabili numeriche utilizzate come indici.

--- Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/90_CicliFunzionatiConItem.js
+++ Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/90_CicliFunzionatiConItem.js
@@ -1,24 +1,38 @@
-
 let blocchi = document.getElementById('ElencoBlocchi'),
     nodiElemento = blocchi.childNodes,
     nodiFiglio = blocchi.children;
 
-// console.log("Ciclo For, utilizzo di item su childNodes");
-// for (let i = 0; i < nodiElemento.length; i++) {
-//   console.log(nodiElemento.item(i));
-// };
-//
-// console.log("Ciclo For, utilizzo di item su children");
-// for (let i = 0; i < nodiFiglio.length; i++) {
-//   console.log(nodiFiglio.item(i));
-// };
-
-console.log("Ciclo for in, utilizzo di item su children");
-for (let index in nodiFiglio) {
-console.log(nodiFiglio.item(index));
+console.log("\nCiclo for, utilizzo di item su children");
+for (let i = 0; i < nodiFiglio.length; i++) {
+  console.log(nodiFiglio.item(i));
 };
 
+// K: si può utilizzare for..in circoscrivendo solo le proprietà enumenrabili numeriche
+
+console.log("\nKim: Ciclo for...in, utilizzo di item su children");
+for (let i in nodiFiglio) {
+  if (i/i==1 || i == 0) { //circoscrivo le proprietà enumerabili numeriche
+  console.log(nodiFiglio.item(i));
+  }
+};
+
+console.log("\nCiclo for, utilizzo di item su childNodes");
+for (let i = 0; i < nodiElemento.length; i++) {
+  console.log(nodiElemento.item(i));
+};
+
+console.log("\nKim: Ciclo for...in, utilizzo di item su childNodes");
+for (let i in nodiElemento) {
+  if (i/i==1 || i == 0) { //circoscrivo le proprietà enumerabili numeriche
+  console.log(nodiElemento.item(i));
+}
+};
+
+
+
+
 /*
+
 Il for..in itera sulle proprietà enumerabili proprie ed ereditate dell'oggetto:
 
 console.log("Ciclo for in, utilizzo di item su childNodes");
@@ -27,19 +41,23 @@ for (let index in nodiElemento) {
 };
 Se esegui questo codice vedrai che index non è un indice che puoi utilizzare con
 il metodo item(), ma contiene di volta in volta una proprietà.
-*/
 
-console.log("Ciclo for in, utilizzo di item su childNodes");
-for (let index in nodiElemento) {
-if (index/index==1 || index == 0) {
-console.log(nodiElemento.item(index));
-}
+console.log("Ciclo for...in, utilizzo di item su children");
+
+for (let index in nodiFiglio) {
+console.log(nodiFiglio.item(index));
 };
 
-console.log("Gianluca, Ciclo for in, utilizzo di item su childNodes");
+console.log("\nGianluca: Ciclo for...in, utilizzo di item su childNodes:");
 for (let index in nodiElemento) {
     console.log(index);
 };
+
+console.log("\nGianluca: Ciclo for...in, utilizzo di item su children:");
+for (let index in nodiFiglio) {
+    console.log(index);
+};
+*/
 
 //non funzionanti
 
