65263255ccfd9bf0a3d8cc6e52f050182653c38f
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-09-28 13:07:21 +0200

    addEventListener

--- Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/95_Introduzione_agli_eventi_secoda_parte.js
+++ Lezioni/S8_Manipolazione_del_DOM_e_gestione_eventi/95_Introduzione_agli_eventi_secoda_parte.js
@@ -1,13 +1,14 @@
 // document.addEventListener('click',(event)=>{
 //   console.log(event.target.innerText);
 // });
-// let car = [];
+let ins = [];
 const inp = document.getElementById('carattere');
 inp.addEventListener('keypress',(pippo)=>{
   // car.push(pippo.which);
   // console.log(pippo.which);
   if (pippo.which === 13) {
     let inpVal = inp.value;
+    ins.push(inpVal);
     inp.value = '';
     console.log(`Hai digitato: ${inpVal}`);
   }
