e43ef69d457326e4047913deab43f0362e41ef65
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-10-14 07:24:14 +0200

    Variazone del capitolo closure e contrassegnato come incomplete le lezioni sulle classi

--- Lezioni/S9_JavaScript_Engine/114_Closure.js
+++ Lezioni/S9_JavaScript_Engine/114_Closure.js
@@ -4,7 +4,23 @@
 // }
 // fn(5);
 // --------------------------------------
-function fn(a){
+// function a1(a){
+//   var aFattore = 1.5;
+//   return () => {
+//     var aCalcolo = (aFattore*a)-10;
+//     console.log(aCalcolo);
+//   }
+// }
+//
+// var a1a = a1(100),
+//     a1b = a1(200);
+
+// a1a();
+// a1b();
+
+// console.log(window);
+// ----------------------------------------
+function afn1(a){
   let fattore = 1.5;
   return () => {
     let calcolo = (fattore*a)-10;
@@ -12,7 +28,10 @@ function fn(a){
   }
 }
 
-const rif = fn(100);
-const rif2 = fn(200);
-rif();
-rif2();
+var a1a = afn1(100),
+    a1b = afn1(200);
+
+a1a();
+a1b();
+
+console.log(window);
