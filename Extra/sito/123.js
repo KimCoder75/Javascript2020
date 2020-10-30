08d2ce730197674258c3dd6c1daeaf75f996041c
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-10-30 14:16:23 +0100

    Aggiornamento

--- Extra/sito/123.js
+++ Extra/sito/123.js
@@ -1,34 +1,47 @@
 const btn = document.getElementById('btn'),
       sezione = document.getElementById('utentiSection');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
 
   const xhr = new XMLHttpRequest();
-  xhr.open('GET','utenti.json',true);
+  xhr.open('GET','utenti.json', true);
   xhr.send();
 
   xhr.onload = function (){
 
-    if (xhr.status===200) {
+    if (xhr.status===200 && xhr.readyState ===4) {
         console.log('');
-        const utentiJSON = xhr.responseText;
-        const utenti = JSON.parse(utentiJSON);
+        let utentiJSON = xhr.responseText,
+            utenti = JSON.parse(utentiJSON);
+        console.clear();
+        console.log(utentiJSON);
+        console.log(utenti);
         showUtenti(utenti);
     }
-
   }
-
 });
 
 function showUtenti(utenti){
-
-  if (utenti.lenght >0) {
+  sezione.textContent='';
+  if (utenti.length > 0) {
     utenti.forEach((utente, i) => {
       showUtente(utente);
     });
-  }
-
-  else {
+  } else{
     sezione.textContent ='Nessun utente da mostrare';
-  }
+    }
+};
+
+function showUtente(utente) {
+  let newUserDiv = document.createElement('div');
+  newUserDiv.style.border = "1 px solid #999";
+  newUserDiv.style.marginTop = "10px";
+  newUserDiv.style.padding = "10px";
+  newUserDiv.innerHTML = `
+  nome: ${utente.nome}<br>
+  anni: ${utente.anni}<br>
+  lavoro: ${utente.lavoro}<br>
+  `;
+  sezione.appendChild(newUserDiv);
+
 };
