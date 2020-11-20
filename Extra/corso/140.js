d966a4e2cb46fa7edbbfe9011d63c97b9ea18132
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-11-20 13:45:03 +0100

    Generatore nomi univoci

--- Extra/corso/140.js
+++ Extra/corso/140.js
@@ -2,4 +2,24 @@
 * Esercitazione: Creazione utenti con nomi non duplicati
 */
 const nomi = ['Simone', 'Sandro', 'Miriam', 'Bianca', 'Eleonora'],
-      cognomi = ['Rossi', 'Del Greco', 'Dileo', 'Ferrari.', 'Morelli'];
+      cognomi = ['Rossi', 'Del Greco', 'Dileo', 'Ferrari', 'Morelli'];
+
+function *generaRandomUser(nomi, cognomi){
+  while(nomi.length > 0){
+    const nomeIndex = (Math.random()*nomi.length) | 0,
+          cognomeIndex = (Math.random()*cognomi.length) | 0,
+          nome = nomi[nomeIndex],
+          cognome= cognomi[cognomeIndex];
+          // console.log(nomeIndex,cognomeIndex,nome,cognome);
+
+    nomi.splice(nomeIndex, 1);
+    // console.log(nomi);
+    cognomi.splice(cognomeIndex, 1);
+    // console.log(cognomi);
+    yield { nome, cognome };
+  }
+}
+
+for(let user of generaRandomUser(nomi, cognomi)){
+  console.log(user);
+}
