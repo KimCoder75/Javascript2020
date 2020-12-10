0d78658b9362d5c6780e1d978eb407d6d97fb6c2
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-12-10 18:01:42 +0100

    Automatica DOM

--- Extra/corso/progetto2/DOM.js
+++ Extra/corso/progetto2/DOM.js
@@ -5,12 +5,12 @@
 
 //Scrivere una funzione che crei elementi del dom
 
-function DOM_Creator(anchor, node, attribute, text, child ) {
+function DOM_Creator(anchor, node, attribute, text, child) {
   let parent = document.getElementById(anchor),
-      eNode = parent.createElement(node),
-      eAttribute parent.setAttribute('class', attribute),
-      eText = parent.innerText(text);
-      
+    eNode = parent.createElement(node),
+    eAttribute parent.setAttribute('class', attribute),
+    eText = parent.innerText(text);
+
   eNode.appendChild(eAttribute);
   eNode.appendChild(eText);
   anchor.appendChild(eNode)
