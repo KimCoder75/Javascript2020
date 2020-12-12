4b5e362650c039e5b089f328d8fdbb00221c383b
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-12-12 09:29:08 +0100

    lib2

--- Extra/corso/progetto2/lib.js
+++ Extra/corso/progetto2/lib.js
@@ -1,14 +1,16 @@
-// this.scrollHeight - this.scrollTop === this.clientHeight
-/*jshint -W030, -W119, -W104*/
+/*jshint -W030, -W033, -W119, -W104*/
 /*jshint expr:true*/
 const blog = {
   itemPerPage: 5,
-  currentPageKeyVar: 0,
+  temporaryPage: 0,
   totalPages: 0,
   blogWrapper: document.getElementById('blog-w'),
   pagePositionWrapper: document.getElementById('page-position-w'),
-  posts: []
+  posts: [],
+  areAllpostsShown: false
 };
+
+window.addEventListener('scroll', setBookmarkPosition);
 
 async function initBlog() {
   const postData = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -17,28 +19,79 @@ async function initBlog() {
   blog.totalPages = Math.ceil(blog.posts.length / blog.itemPerPage);
   initBookmarks();
   showPosts();
+  checkCodeBlocks()
+}
+
+function checkCodeBlocks() {
+  let scrollTop, scrollHeight, scrollTopMax, clientHeight, temporaryPage, totalPages, areAllpostsShown;
+  ({
+    scrollTop,
+    scrollHeight,
+    scrollTopMax,
+    clientHeight
+  } = document.documentElement);
+  ({
+    temporaryPage,
+    totalPages,
+    areAllpostsShown
+  } = blog);
+  console.clear();
+  console.log(`Debug:
+		scrollTop: ${scrollTop}
+		scrollHeight: ${scrollHeight}
+		clientHeight: ${clientHeight}
+		scrollTopMax: ${scrollTopMax}
+		scrollHeight - clientHeight: ${scrollHeight - clientHeight}
+		(scrollHeight - clientHeight) === scrollTopMax: ${(scrollHeight - clientHeight) === scrollTopMax}
+		temporaryPage: ${temporaryPage}
+		totalPages-1: ${totalPages-1}
+		areAllpostsShown: ${areAllpostsShown}
+		altezza pagina: ${((scrollTopMax)/(totalPages-1))}
+		altezza pagina2: ${((scrollHeight-36)/(temporaryPage+1))}
+		altezza pagina3: ${(scrollHeight-36)/5}
+		altezza pagina4: ${((scrollTop)/5)-36}
+		`);
 }
 
 function initBookmarks() {
   for (let i = 0; i < blog.totalPages; i++) {
     let classes = 'position';
-    (i == blog.currentPageKeyVar) ? classes += ' active': '';
+    (i == blog.temporaryPage) ? classes += ' active': '';
     let span = document.createElement('span');
     span.setAttribute('class', classes);
     blog.pagePositionWrapper.appendChild(span);
   }
 }
 
-//TODO: si basa tutto su currentPageKeyVar, impostare bene currentPageKeyVar
-function showPosts() {
-  let start, currentPageKeyVar, itemPerPage, blogWrapper, posts;
+function setBookmarkPosition() {
+  let scrollTop, scrollTopMax, temporaryPage, totalPages;
   ({
-    currentPageKeyVar,
+    scrollTop,
+    scrollTopMax
+  } = document.documentElement);
+  ({
+    temporaryPage,
+    totalPages
+  } = blog);
+  if ((scrollTop >= scrollTopMax) && (temporaryPage < (totalPages - 1))) {
+    blog.temporaryPage++;
+    showPosts();
+  }
+  checkCodeBlocks();
+}
+
+//TODO: si basa tutto su temporaryPage, impostare bene temporaryPage
+function showPosts() {
+  let start, temporaryPage, itemPerPage, blogWrapper, posts, areAllpostsShown, totalPages;
+  ({
+    temporaryPage,
     itemPerPage,
     blogWrapper,
-    posts
+    posts,
+    areAllpostsShown,
+    totalPages
   } = blog);
-  start = currentPageKeyVar * itemPerPage;
+  start = temporaryPage * itemPerPage;
   for (let i = start; i < start + itemPerPage; i++) {
 
     let DOM_post = document.createElement('article'),
@@ -59,7 +112,15 @@ function showPosts() {
 
     blogWrapper.appendChild(DOM_post);
   }
+  if (temporaryPage === (totalPages - 1)) {
+    blog.areAllpostsShown = true;
+    // setCurrentPage();
+  }
 }
+
+// setCurrentPage() {
+//
+// }
 
 function addPosts(i, h3, div1, div2, posts) {
   h3.innerText = posts[i].title;
