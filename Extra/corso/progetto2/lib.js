bb33b162a640f1f1d11b25bbe5cd4e1c625b7677
KimCoder <63841698+KimCoder75@users.noreply.github.com>
2020-12-15 07:39:19 +0100

    refactoring 2.0

--- Extra/corso/progetto2/lib.js
+++ Extra/corso/progetto2/lib.js
@@ -1,75 +1,93 @@
 /*jshint -W030,-W033,-W119,-W104*/
 /*jshint expr:true*/
 const blog = {
-	itemsPerPage : 5,
-	currentPage: 0,
-	totalePage: 0,
-	blogWrapper : document.getElementById('blog-w'),
-	pagePositionWrapper : document.getElementById('page-position-w'),
-	arePostCharged : false,
-	flag : 0
+	postPerPage: 5,
+	totalPages: 0,
+	bookmarksW: document.getElementById('page-position-w'),
+	postsW: document.getElementById('blog-w'),
+	posts: [],
+	totalPosts: 25,
+	activeBookmark: 0
 }
 
-window.addEventListener('scroll', (e)=>{
-	let scrollHeight, clientHeight, scrollTop, scrollTopMax;
-	({scrollHeight, clientHeight, scrollTop, scrollTopMax} = document.documentElement);
-	console.log(scrollTopMax);
-	if ((scrollTop >= scrollTopMax - 1) && (blog.currentPage < blog.totalPage -1)) {
-		blog.currentPage++
-		showPosts(scrollHeight, clientHeight, scrollTop, scrollTopMax);
-		// logs();
-		setIndicatoreAttivo();
+window.addEventListener('scroll', scrollManager);
+
+function scrollManager() {
+	let scrollTop, scrollTopMax, clientHeight, scrollHeight;
+	({
+		scrollTop,
+		scrollTopMax,
+		clientHeight,
+		scrollHeight
+	} = document.documentElement);
+	if (blog.posts.length > 0 && (scrollTop / scrollTopMax )>= 1) {
+		blog.posts.splice(0, 5);
+		blog.activeBookmark ++
+		console.log(blog.activeBookmark);
+
+		initPages();
 	}
-});
+	// 40 è la somma del padding-top e padding-bottom
+	// blog.activeBookmark = Math.floor(scrollTop / (clientHeight - 40));
+	console.clear();
+	console.log('T/M:',((scrollTop / (scrollTopMax))*100));
+	setBookmarks();
+}
 
-// function logs(scrollHeight, clientHeight, scrollTop, scrollTopMax){
-// 	if (blog.currentPage === (blog.totalPage -1)) {
-// 		blog.arePostCharged = true;
-// 		blog.flag = (scrollTop/scrollTopMax);
-// 		console.log('scrollTop:',scrollTop,'scrollTopMax:',scrollTopMax,'Flag:',blog.flag);
-// 	}
-// }
+async function initBlog() {
+	const importedPosts = await fetch('https://jsonplaceholder.typicode.com/posts');
+	blog.posts = await importedPosts.json();
+	blog.posts.length = blog.totalPosts;
+	blog.totalPages = Math.ceil(blog.totalPosts / blog.postPerPage);
 
-function setIndicatoreAttivo(){
-	let indicatori = document.querySelectorAll('span.position');
-	indicatori.forEach((indicatore, i) => {
-		(i === blog.currentPage)? indicatore.setAttribute('class','position active'):indicatore.setAttribute('class','position');
+	initBookmark();
+	initPages();
+}
+
+function bookmarkActivator(index, ref, anchor) {
+	(index === ref) ? anchor.setAttribute('class', 'position active'): anchor.setAttribute('class', 'position');
+}
+
+function initBookmark() {
+	for (let i = 0; i < blog.totalPages; i++) {
+		let span = document.createElement('span');
+		bookmarkActivator(i, 0, span);
+		blog.bookmarksW.appendChild(span)
+	}
+}
+
+function setBookmarks() {
+	let bookmarks = document.querySelectorAll('span.position')
+	bookmarks.forEach((bookmark, i) => {
+		bookmarkActivator(i, blog.activeBookmark, bookmark);
 	});
 }
 
-async function initBlog(){
-	const postData = await fetch('https://jsonplaceholder.typicode.com/posts');
-	blog.posts = await postData.json();
-	blog.posts.splice(25,75);
-	blog.totalPage = Math.ceil(blog.posts.length/blog.itemsPerPage);
-	initIndicatoriPaginazione();
-	showPosts();
-}
+function initPages() {
+	if (blog.posts.length !== 0) {
+		for (let i = 0; i < blog.postPerPage; i++) {
 
-function initIndicatoriPaginazione(){
-	for (let i = 0; i < blog.totalPage; i++) {
-		let pagination = document.createElement('span');
-		(i === 0)? pagination.setAttribute('class','position active'):pagination.setAttribute('class','position');
-		blog.pagePositionWrapper.appendChild(pagination);
+			const article = document.createElement('article'),
+				h3 = document.createElement('h3'),
+				div1 = document.createElement('div'),
+				div2 = document.createElement('div');
+
+			article.setAttribute('class', 'blog-post');
+			h3.setAttribute('class', 'title');
+			div1.setAttribute('class', 'body');
+			div2.setAttribute('class', 'id');
+
+			h3.innerText = blog.posts[i].title;
+			div1.innerText = blog.posts[i].body;
+			div2.innerText = blog.posts[i].id;
+
+			article.appendChild(h3);
+			article.appendChild(div1);
+			article.appendChild(div2);
+
+			blog.postsW.appendChild(article);
+		}
 	}
 }
-
-function showPosts(){
-	let start = blog.currentPage * blog.itemsPerPage;
-	for (let i = start; i < start + blog.itemsPerPage; i++) {
-		let DOM_post = document.createElement('article');
-		DOM_post.setAttribute('class', 'blog-post');
-		DOM_post.innerHTML = createPostHTML(blog.posts[i]);
-		blog.blogWrapper.appendChild(DOM_post);
-	}
-}
-
-function createPostHTML({ id, title, body }){
-	return `<h3 class='title'>${title}</h3>
-	<div class ='body'>${body}</div>
-	<div class ='id'>${id}</div>`
-}
-
-
 
 initBlog();
