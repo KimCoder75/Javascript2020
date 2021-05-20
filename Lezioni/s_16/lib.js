a5e35a692b38cb5e9061b7cd166437e5847fa007
KimCoder75 <63841698+KimCoder75@users.noreply.github.com>
2021-05-20 10:51:33 +0200

    Prima pagina html

---
+++ Lezioni/s_16/lib.js
@@ -0,0 +1,15 @@
+
+/*
+if(! ('sessionStorage' in window)){
+  window.sessionStorage = {
+    store :{},
+    setItem: function() {},
+    getItem: function() {}
+  };
+}
+*/
+// Transpilazione
+// file di sviluppo js
+let str = `I template literal sono stati introdotti in ES6`;
+// file di produzione js
+var str2 = 'I template literal sono stati introdotti in ES6';
--- Progetti/04_CountDown/lib.js
+++ Progetti/04_CountDown/lib.js
@@ -1,21 +1,21 @@
 const indicatori = document.querySelectorAll('#time span'),
-      scadenza = new Date(2021, 5, 8, 16, 30, 0),
-      ms_scadenza = scadenza.getTime(),
-      ms_in_giorno = (24*60*60*1000),
-      ms_in_ora = (60*60*1000),
-      ms_in_minuto = (60*1000);
+  scadenza = new Date(2021, 5, 8, 14, 00, 0),
+  ms_scadenza = scadenza.getTime(),
+  ms_in_giorno = (24 * 60 * 60 * 1000),
+  ms_in_ora = (60 * 60 * 1000),
+  ms_in_minuto = (60 * 1000);
 
-function countdown(){
+function countdown() {
   const data_corrente = new Date(),
-        ms_corrente = data_corrente.getTime(),
-        ms_rimanenti = ms_scadenza - ms_corrente;
+    ms_corrente = data_corrente.getTime(),
+    ms_rimanenti = ms_scadenza - ms_corrente;
   let giorni = ~~(ms_rimanenti / ms_in_giorno),
-      ore = ~~((ms_rimanenti % ms_in_giorno ) / ms_in_ora),
-      minuti = ~~((ms_rimanenti % ms_in_ora) / ms_in_minuto),
-      secondi = ~~((ms_rimanenti % ms_in_minuto) / 1000);
+    ore = ~~((ms_rimanenti % ms_in_giorno) / ms_in_ora),
+    minuti = ~~((ms_rimanenti % ms_in_ora) / ms_in_minuto),
+    secondi = ~~((ms_rimanenti % ms_in_minuto) / 1000);
   let time = [giorni, ore, minuti, secondi];
 
-  if(ms_rimanenti <= 0){
+  if (ms_rimanenti <= 0) {
     clearInterval(contdownInterval);
     return;
   }
